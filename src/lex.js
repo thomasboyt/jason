54a00860dd228cd2c8529877ac9fdf37541cce27
Thomas Boyt <thomas.boyt@venmo.com>
2014-04-10 19:36:10 -0400

    Helper for creating lexer functions

--- src/lex.js
+++ src/lex.js
@@ -32,40 +32,44 @@ let Token = function(type, value) {
   this.value = value;
 };
 
-// Lexing
-// =================
 
-let lexString = function(input, acc = '') {
-  let char = input[0];
-  let rest = input.slice(1);
-
-  // TODO: a bunch of weird control chars
-  return match(
-    char,
-    () => lexString(rest, acc + char),
-    [QUOTE, () => [new Token('string', acc), rest]],
-    [undefined, () => { throw new Error('No matching quote found for string'); }]
-  );
-};
-
-let lexAlpha = function(input, acc = '') {
-  let char = input[0];
-  let rest = input.slice(1);
-
-  return match(
-    char,
-    () => match(
-      acc,
-      () => { throw new Error('Unexpected token ' + acc); },
-      [TRUE, () => [new Token(TRUE), rest]],
-      [FALSE, () => [new Token(FALSE), rest]],
-      [NULL, () => [new Token(NULL), rest]]
-    ),
-    [ALPHA, () => lexAlpha(rest, acc + char)]
-  );
-};
+function makeLexFn(cb) {
+  return function(input, acc) {
+    let cur = input[0];
+    let rest = input.slice(1);
+    return cb(cur, rest, acc);
+  };
+}
 
 /**
+ * Lexing strings
+ */
+let lexString = makeLexFn((cur, rest, acc) => match(
+  cur,
+  () => lexString(rest, (acc || '') + cur),
+  [QUOTE, () => [new Token('string', acc), rest]],
+  [undefined, () => { throw new Error('No matching quote found for string'); }]
+));
+
+/**
+ * Lexing true, false, and null
+ */
+let lexAlpha = makeLexFn((cur, rest, acc) => match(
+  cur,
+  () => match(
+    acc,
+    () => { throw new Error('Unexpected token ' + acc); },
+    [TRUE, () => [new Token(TRUE), rest]],
+    [FALSE, () => [new Token(FALSE), rest]],
+    [NULL, () => [new Token(NULL), rest]]
+  ),
+  [ALPHA, () => lexAlpha(rest, (acc || '') + cur)]
+));
+
+/**
+ * Lexing numbers
+ * See http://www.json.org/number.gif
+ *
  * A number token has:
  *   negative (optional)
  *   int
@@ -74,91 +78,71 @@ let lexAlpha = function(input, acc = '') {
  *   expNegative (optional)
  */
 
-let lexExpPiece = function(input, acc) {
-  let char = input[0];
-  let rest = input.slice(1);
+let lexExpPiece = makeLexFn((cur, rest, acc) => match(
+  cur,
+  () => [new Token('number', m.clj_to_js(acc)), cur + rest],
+  [DIGIT, () => lexExpPiece(rest, m.assoc(acc, 'exp', (m.get(acc, 'exp') || '') + cur))]
+));
 
-  return match(
-    char,
-    () => [new Token('number', m.clj_to_js(acc)), input],
-    [DIGIT, () => lexExpPiece(rest, m.assoc(acc, 'exp', (m.get(acc, 'exp') || '') + char))]
-  );
-};
+let lexExp = makeLexFn((cur, rest, acc) => match(
+  cur,
+  () => [new Token('number', m.clj_to_js(acc)), cur + rest],
+  [PLUS, () => lexExpPiece(rest, acc)],
+  [MINUS, () => lexExpPiece(rest, m.assoc(acc, 'expNegative', true))],
+  [DIGIT, () => lexExpPiece(cur + rest, acc)]
+));
 
-let lexExp = function(input, acc) {
-  let char = input[0];
-  let rest = input.slice(1);
+let lexFracPiece = makeLexFn((cur, rest, acc) => match(
+  cur,
+  () => [new Token('number', m.clj_to_js(acc)), cur + rest],
+  [DIGIT, () => lexFracPiece(rest, m.assoc(acc, 'frac', (m.get(acc, 'frac') || '') + cur))],
+  [E, () => lexExp(rest, acc)]
+));
 
-  return match(
-    char,
-    () => [new Token('number', m.clj_to_js(acc)), input],
-    [PLUS, () => lexExpPiece(rest, acc)],
-    [MINUS, () => lexExpPiece(rest, m.assoc(acc, 'expNegative', true))],
-    [DIGIT, () => lexExpPiece(input, acc)]
-  );
-};
+let lexOptionalDecimal = makeLexFn((cur, rest, acc) => match(
+  cur,
+  () => [new Token('number', m.clj_to_js(acc)), cur + rest],
+  [PERIOD, () => lexFracPiece(rest, acc)],
+  [E, () => lexExp(rest, acc)]
+));
 
-let lexFracPiece = function(input, acc) {
-  let char = input[0];
-  let rest = input.slice(1);
+let lexIntPiece = makeLexFn((cur, rest, acc) => match(
+  cur,
+  () => [new Token('number', m.clj_to_js(acc)), cur + rest],
+  [DIGIT, () => lexIntPiece(rest, m.assoc(acc, 'int', (m.get(acc, 'int') || '') + cur))],
+  [PERIOD, () => lexFracPiece(rest, acc)],
+  [E, () => lexExp(rest, acc)]
+));
 
-  return match(
-    char,
-    () => [new Token('number', m.clj_to_js(acc)), input],
-    [DIGIT, () => lexFracPiece(rest, m.assoc(acc, 'frac', (m.get(acc, 'frac') || '') + char))],
-    [E, () => lexExp(rest, acc)]
-  );
-};
+let lexNumber = makeLexFn((cur, rest, acc) => match(
+  cur,
+  () => { throw new Error('Unexpected token ' + cur); },
+  [ZERO, () => {
+    acc = acc || m.hash_map();
+    return lexOptionalDecimal(rest, m.assoc(acc, 'int', '0'));
+  }],
+  [NONZERO_DIGIT, () => {
+    acc = acc || m.hash_map();
+    return lexIntPiece(cur + rest, acc);
+  }],
+  [MINUS, () => {
+    if ( acc && m.get(acc, 'negative') === true ) {
+      throw new Error('Unexpected token ' + cur);
+    }
+    return lexNumber(rest, m.assoc(acc, 'negative', true));
+  }]
+));
 
-let lexOptionalDecimal = function(input, acc) {
-  let char = input[0];
-  let rest = input.slice(1);
-
-  return match(
-    char,
-    () => [new Token('number', m.clj_to_js(acc)), input],
-    [PERIOD, () => lexFracPiece(rest, acc)],
-    [E, () => lexExp(rest, acc)]
-  );
-};
-
-let lexIntPiece = function(input, acc) {
-  let char = input[0];
-  let rest = input.slice(1);
-
-  return match(
-    char,
-    () => [new Token('number', m.clj_to_js(acc)), input],
-    [DIGIT, () => lexIntPiece(rest, m.assoc(acc, 'int', (m.get(acc, 'int') || '') + char))],
-    [PERIOD, () => lexFracPiece(rest, acc)],
-    [E, () => lexExp(rest, acc)]
-  );
-};
-
-let lexNumber = function(input, acc = m.hash_map()) {
-  // http://www.json.org/number.gif
-
-  let char = input[0];
-  let rest = input.slice(1);
-
-  return match(
-    char,
-    () => { throw new Error('Unexpected token ' + char); },
-    [ZERO, () => lexOptionalDecimal(rest, m.assoc(acc, 'int', '0'))],
-    [NONZERO_DIGIT, () => lexIntPiece(input, acc)],
-    [MINUS, () => {
-      if ( m.get(acc, 'negative') === true ) {
-        throw new Error('Unexpected token ' + char);
-      }
-      return lexNumber(rest, m.assoc(acc, 'negative', true));
-    }]
-  );
-};
-
+/**
+ * Lexing single characters ({ } [ ] : ,)
+ */
 let lexCharacter = function(input) {
   return [ new Token(input[0]), input.slice(1) ];
 };
 
+/**
+ * Main lexer
+ */
 export default function lex(input) {
   // return an array of tokens
   input = ltrim(input);
@@ -186,8 +170,6 @@ export default function lex(input) {
     [ALPHA, () => lexAlpha(input)],
     [NUMBER_START, () => lexNumber(input)]
   );
-
-  console.log(sym);
 
   if ( rest === null ) {
     return [sym];
